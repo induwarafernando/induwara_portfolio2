0ffb0e3779e12dc184794fbbac5feea58c8e3d4b
Induwara Fernando9 <induwarafernando9@gmail.com>
2023-09-06 16:12:20 +0530

    commit

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -1,10 +1,14 @@
 import './Contact.scss'
 
 const Contact = () => {
-  return <div className="contact">
-    <h1>Contact</h1>
+  return (
+  <div className="contact">
+    <h1>Get in Touch</h1>
+    <span>
+        lorem ipsum dolor sit amet consectetur adipisicing elit.
+    </span>
     </div>
-  
+  )
 }
 
 export default Contact
